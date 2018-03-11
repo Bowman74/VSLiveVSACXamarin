763ccc56c95cbc4745cd6109e60c0d857cb38776
Kevin Ford <4380198+Bowman74@users.noreply.github.com>
2018-03-11 14:12:31 -0400

    Initial project checkin.

---
+++ VSACLiveXamarin/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace VSACLiveXamarin
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
